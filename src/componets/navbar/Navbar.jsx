8939d45d7d9fdddca89a314daeff1d5083b9f1e2
Lekiss <lekissdarams7@gmail.com>
2024-09-30 06:59:17 -0700

    modified navbar.jsx

--- src/componets/navbar/Navbar.jsx
+++ src/componets/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import "./Navbar.css"
-import Logo from "../../assets/logo.png"
+import Logo from "../../assets/Logo.png"
 import { Link } from 'react-scroll';
 import menu_icon from "../../assets/menu-icon.png"
 const Navbar = () => {
